e144d943361b23a0fb19c77b4de1f7e52a884842
Mark Faust <markfaust123@gmail.com>
2025-02-24 18:14:46 -0500

    updated prompt and changed AWS Bedrock LLM model to use Mistral which is less supervised than the previously-used Anthropic Claude model

--- image/src/rag_app/query_rag.py
+++ image/src/rag_app/query_rag.py
@@ -5,17 +5,17 @@ from langchain_aws import ChatBedrock
 from rag_app.get_chroma_db import get_chroma_db
 
 PROMPT_TEMPLATE = """
-Answer the question based only on the following context:
+Answer the question, as if you were my financial advisor, based only on the following context:
 
 {context}
 
 ---
 
-Answer the question based on the above context: {question}
+Answer the question, as if you were my financial advisor, based on the above context: {question}
 """
 
-BEDROCK_MODEL_ID = "anthropic.claude-3-haiku-20240307-v1:0"
-
+# BEDROCK_MODEL_ID = "anthropic.claude-3-haiku-20240307-v1:0"
+BEDROCK_MODEL_ID = "mistral.mistral-7b-instruct-v0:2"
 
 @dataclass
 class QueryResponse:
@@ -28,7 +28,7 @@ def query_rag(query_text: str) -> QueryResponse:
     db = get_chroma_db()
 
     # Search the DB.
-    results = db.similarity_search_with_score(query_text, k=3)
+    results = db.similarity_search_with_score(query_text, k=10)
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
