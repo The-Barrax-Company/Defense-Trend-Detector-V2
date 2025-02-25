3353bca84d276083ace3a7286fbf4ddbc30ddfb4
Mark Faust <markfaust123@gmail.com>
2025-02-24 22:18:43 -0500

    refactored out Headerimages

---
+++ app/src/components/HeaderImages.tsx
@@ -0,0 +1,20 @@
+const HeaderImages = () => {
+    return (
+      <>
+        <div>
+          <a href="https://" target="_blank">
+            <img src={"/trend-svgrepo-com.svg"} className="logo" alt="Trend" />
+          </a>
+          <a href="https://" target="_blank">
+            <img
+              src={"/fighter-jet-svgrepo-com.svg"}
+              className="logo"
+              alt="Jet"
+            />
+          </a>
+        </div>
+      </>
+    );
+}
+
+export default HeaderImages
